2c544bc16d50dcccf874463c3f46af05ed0c9ac2
Alexej Yaroshevich <alex@qfox.ru>
2015-01-05 06:05:36 +0400

    form__control: renamed to form__field

--- common.blocks/form/__field/_type/form__field_type_button.js
+++ common.blocks/form/__field/_type/form__field_type_button.js
@@ -3,7 +3,7 @@ modules.define('form', function(provide, Form) {
  * Контрол типа button (кнопка)
  * Подмешивается к блоку button
  */
-provide(Form.decl({ block : this.name, elem : 'control', modName : 'type', modVal : 'button' }, {
+provide(Form.decl({ block : this.name, elem : 'field', modName : 'type', modVal : 'button' }, {
     /**
      * Заглушка
      */
--- common.blocks/form/__field/form__field.bh.js
+++ common.blocks/form/__field/form__field.bh.js
@@ -1,6 +1,6 @@
 module.exports = function(bh) {
 
-    bh.match('form__control', function(ctx) {
+    bh.match('form__field', function(ctx) {
 
         ctx.js(true);
 
