102ef0b00f8a4a7b1595ca3a28726a73a471697f
Anton Winogradov <winogradovaa@gmail.com>
2015-01-13 18:14:00 +0300

    add form example

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -3,20 +3,68 @@
     title : 'form',
     styles : [{ elem : 'css', url : 'index.min.css' }],
     scripts : [{ elem : 'js', url : 'index.min.js' }],
-    content : {
-        block : 'form',
-        method : 'GET',
-        content : [
-            {
-                block : 'input',
-                mods : { theme : 'islands', size : 'l' },
-                name : 'text'
-            },
-            {
-                block : 'button',
-                mods : { theme : 'islands', size : 'l', type : 'submit' },
-                text : 'Найти'
-            }
-        ]
-    }
+    content : [
+        {
+            block : 'form',
+            mods : { theme : 'islands' },
+            method : 'GET',
+            content : [
+                {
+                    elem : 'content',
+                    content : [
+                        {
+                            elem : 'field',
+                            mods : { type : 'input' },
+                            content : [
+                                {
+                                    block : 'label',
+                                    mods : { theme : 'islands' },
+                                    content : 'Input'
+                                },
+                                {
+                                    block : 'input',
+                                    mods : { theme : 'islands', size : 'l' },
+                                    name : 'input'
+                                }
+                            ]
+                        },
+                        {
+                            elem : 'field',
+                            mods : { type : 'checkbox' },
+                            content : [
+                                {
+                                    block : 'checkbox',
+                                    mods : { theme : 'islands', size : 'l' },
+                                    text : 'Checkbox 1',
+                                    name : 'checkbox'
+                                }
+                            ]
+                        },
+                        {
+                            elem : 'field',
+                            mods : { type : 'radio' },
+                            content : [
+                                {
+                                    block : 'radio',
+                                    mods : { theme : 'islands', size : 'l' },
+                                    text : 'Radio 1',
+                                    name : 'radio'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    elem : 'footer',
+                    content : [
+                        {
+                            block : 'button',
+                            mods : { theme : 'islands', size : 'l', type : 'submit', view : 'action' },
+                            text : 'Submit'
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 })
