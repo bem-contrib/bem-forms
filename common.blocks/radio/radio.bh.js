cd31c70f9a7f3d7604b9903a0a9aec4bba3240df
Alexej Yaroshevich <zxqfox@gmail.com>
2015-03-24 02:18:44 +0700

    form-field: bh templates for passing name to controls

---
+++ common.blocks/radio/radio.bh.js
@@ -0,0 +1,11 @@
+module.exports = function (bh) {
+
+    bh.match('radio', function (ctx, json) {
+	var _form_field = ctx.tParam('_form_field');
+
+	if(_form_field) {
+	    json.name = json.name || _form_field.name;
+	}
+    });
+
+};
