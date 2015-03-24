3a573066e8338870b9d2b214e8d24cec11355eb6
verybigman <winogradovaa@gmail.com>
2015-03-24 14:55:23 +0300

    hidden fallback and jscs fixes

--- common.blocks/checkbox/checkbox.bh.js
+++ common.blocks/checkbox/checkbox.bh.js
@@ -1,11 +1,11 @@
-module.exports = function (bh) {
+module.exports = function(bh) {
 
-    bh.match('checkbox', function (ctx, json) {
-	var _form_field = ctx.tParam('_form_field');
+    bh.match('checkbox', function(ctx, json) {
+        var _form_field = ctx.tParam('_form_field');
 
-	if(_form_field) {
-	    json.name = json.name || _form_field.name;
-	}
+        if(_form_field) {
+            json.name = json.name || _form_field.name;
+        }
     });
 
 };
