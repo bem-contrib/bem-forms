7dae78e87a525cc7da626537db0c6967ee237f9a
voischev <voischev.ivan@ya.ru>
2015-04-15 17:41:46 +0300

    spec validation emali

---
+++ common.blocks/validation/_email/validation_email.spec.js
@@ -0,0 +1,25 @@
+modules.define('spec', ['validation', 'validation_email'], function(provide, Validation, ValidationEmail) {
+
+    describe('validation_email', function() {
+
+        var message = 'Field requires email inside',
+            validators;
+        beforeEach(function() {
+            validators = Validation.create();
+            validators.push(new ValidationEmail({
+                message : message
+            }));
+        });
+
+        it('should contain @', function() {
+            (validators.check('test@test.test') === null).should.be.true;
+        });
+
+        it('should show validation message', function() {
+            (validators.check('ololo') || '').should.equal(message);
+        });
+
+    });
+
+    provide();
+});
