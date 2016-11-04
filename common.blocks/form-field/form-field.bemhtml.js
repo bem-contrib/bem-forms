1d45b7024be983e03dd97f8f97d00a3e2461f02c
adinvadim <adin.vadim@gmail.com>
2016-11-04 17:20:03 +0300

    feat(form-field) Label and control generator

--- common.blocks/form-field/form-field.bemhtml.js
+++ common.blocks/form-field/form-field.bemhtml.js
@@ -1,21 +1,39 @@
 block('form-field')(
 
-    js()(true),
-
     def()(function() {
         var _form_field = this.ctx;
-        _form_field.id = _form_field.id || this.generateId();
-        this.ctx.js = this.extend(this.ctx.js || {}, { id : _form_field.id });
 
+        _form_field.id = _form_field.id || this.generateId();
         _form_field.mods = _form_field.mods || {};
 
         return applyNext({ _form_field : _form_field });
+    }),
+
+    js()(function() {
+        return this.extend(applyNext(), { id : this._form_field.id });
     }),
 
     attrs()(function () {
         return {
             'data-name' : this.ctx.name
         };
+    }),
+
+    match(function() { return this.ctx.label !== undefined; }).content()(function() {
+        return [
+            {
+                block : 'label',
+                content : this.ctx.label
+            },
+            applyNext()
+        ];
+    }),
+
+    match(function() { return this.ctx.control !== undefined; }).content()(function() {
+        return [
+            applyNext(),
+            this.ctx.control
+        ];
     })
 
 );
